a24daac43fecfd812944a0cc77ba2b6224750111
cavichiolimariana@gmail.com <cavichiolimariana@gmail.com>
2018-12-10 17:34:37 -0200

    Alterando a base de dados

--- Classificacao.R
+++ Classificacao.R
@@ -1,36 +1,34 @@
+library('caret')
 library('rpart')
-library('rpart.plot') 
-source('Metricas.R')
+library('rpart.plot')
 
-classificacao <- function(){
-	# Leitura de dados de um arquivo com linha de cabeçalho e separador ','
-	dados <- read.table('Autism-Child-Data.csv', header = TRUE, sep = ',')
-
-	sample <- createDataPartition(dados$Class, p = 0.75, list = FALSE)
-  	trainSample <- dados[sample,]
-  	testSample <- dados[-sample,]
-
-  	modelo <- rpart(Class ~ A1_Score + A2_Score + A3_Score + A4_Score + A5_Score + A6_Score + A7_Score + A8_Score + 
-  		A9_Score + A10_Score + age_numeric + gender + ethnicity + jundice + austim + contry_of_res + used_app_before + 
-  		result_numeric + age_desc + relation, 
-  		data = trainSample, method = "class", control = rpart.control(minsplit = 1), params = list(split = "Information"), model = TRUE)
-
-  	predicao <- predict(modelo, testSample, type = "class")
-
-  	matriz <- table(predicted = predicao, truth = testSample$Class)
-  	acuracia <- acuracia(matriz)
-  	precisao <- precisao(matriz)
-
-  	dados_coletados <- list()
-	dados_coletados$modelo <- modelo
-	dados_coletados$acuracia <- acuracia
-	dados_coletados$precisao <- precisao
-
-	return(dados_coletados)
+classificacao <- function() {
+  dados <- read.table('mammographic.csv', header = TRUE, sep = ",")
+  sample <- createDataPartition(dados$Severity, p = 0.75, list = FALSE)
+  
+  trainSample <- dados[sample,]
+  testSample <- dados[-sample,]
+  
+  modelo <- rpart(formula = Severity ~ BI_RADS + Age + Shape + Margin + Density,
+                 data = trainSample, method = "class", control = rpart.control(minsplit = 1), 
+                 parms = list(split = "Information"), model = TRUE)
+  
+  predicao <- predict(modelo, testSample, type = "class")
+  
+  matriz <- table(predicted=predicao, truth=testSample$Severity)
+  acuracia <- sum(diag(matriz))/sum(matriz) 
+  precisao <- sum(matriz[2,2])/sum(matriz[2,])
+  
+  resultado <- list()
+  resultado$modelo <- modelo
+  resultado$acuracia <- acuracia
+  resultado$precisao <- precisao
+  
+  return(resultado)
 }
 
-classificacao <- classificacao()
+resultado <- classificacao()
 cat("Método de Classificação:\n")
-cat("Acurácia: ",classificacao$acuracia,"\n" )
-cat("Precisão: ",classificacao$precisao,"\n\n")
-rpart.plot(classificacao$modelo)
+cat("Acurácia: ",resultado$acuracia,"\n" )
+cat("Precisão: ",resultado$precisao,"\n\n")
+rpart.plot(resultado$modelo)  
